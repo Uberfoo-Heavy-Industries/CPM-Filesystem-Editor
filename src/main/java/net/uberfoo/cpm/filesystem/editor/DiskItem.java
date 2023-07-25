142e0a19063ea673464f314c43e24709c6c4567e
James Bryant <13969279+james-bryant@users.noreply.github.com>
2023-07-25 18:23:49 -0500

    CPMU-30: boot track support

--- src/main/java/net/uberfoo/cpm/filesystem/editor/DiskItem.java
+++ src/main/java/net/uberfoo/cpm/filesystem/editor/DiskItem.java
@@ -3,10 +3,18 @@ package net.uberfoo.cpm.filesystem.editor;
 import javafx.beans.property.BooleanProperty;
 import net.uberfoo.cpm.filesystem.CpmDisk;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 public interface DiskItem extends AcceptsImports {
 
     CpmDisk disk();
 
     BooleanProperty dirtyProperty();
+
+    default void importBootTracks(ByteBuffer buffer) throws IOException {
+        disk().writeBootTracks(buffer);
+        dirtyProperty().setValue(true);
+    }
 
 }
